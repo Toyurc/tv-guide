24c8245dcf8a3799aa931d8eb65ea9383bc1769a
toyurc-vmn <t.adebayo-ige@verifyme.ng>
2020-07-09 17:02:23 +0100

    Feat: adds loader, and search page

--- src/App.js
+++ src/App.js
@@ -7,18 +7,19 @@ import {
 } from 'react-router-dom';
 
 import './App.scss';
+import Loader from './components/Loader';
 const SearchDetails = React.lazy(() => import('./pages/SearchPage/index'));
 const SearchPage = React.lazy(() => import('./pages/SearchDetails/index'));
 
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loader />}>
         <Router>
           <div id="body">
             <Switch>
               <Route path='/' exact component={SearchDetails} />
-              <Route path='/?show=:searchterm' exact component={SearchPage} />
+              <Route path='/show=:searchterm' exact component={SearchPage} />
             </Switch>
           </div>
         </Router>
--- src/pages/SearchPage/searchPage.jsx
+++ src/pages/SearchPage/searchPage.jsx
@@ -1,22 +1,56 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import { ReactComponent as SearchIcon } from "../../assets/svg/seacrIcon.svg";
+
+import "./searchPage.scss";
 
 const SearchPage = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [error, setError] = useState(false);
+
+  const history = useHistory();
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    const isEmpty = /^(\w+\S+)$/.test(searchTerm);
+    if (!isEmpty) {
+      setError(true);
+
+      setTimeout(() => {
+        setError(false);
+      }, 3000);
+    }
+
+    history.push(`/show=${searchTerm}`);
+  };
 
   return (
     <section>
-      <div>
-        <h4>Hi there! search for your favourite shows</h4>
-        <form method="POST">
-          <input
-            onChange={(e) => {setSearchTerm(e.target.value)}}
-            type="search"
-            value={searchTerm}
-            id="site-search"
-            name="q"
-            aria-label="Search for your favourite shows"
-          />
-          <button type="submit">Find {searchTerm}</button>
+      <div className="container">
+        <h2 className="heading">Hi there! search for your favourite shows</h2>
+        <form
+          onSubmit={(e) => {
+            submitForm(e);
+          }}
+        >
+          <div className={`searchBox ${error ? "searchBox-error" : ""}`}>
+            <SearchIcon />
+            <input
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+              }}
+              type="search"
+              value={searchTerm}
+              className="inputBox"
+              id="site-search"
+              name="q"
+              aria-label="Search for your favourite shows"
+            />
+          </div>
+          <button className="button" type="submit">
+            Find {searchTerm}
+          </button>
         </form>
       </div>
     </section>
